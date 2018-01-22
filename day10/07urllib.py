6f7e4a1f28f5834ef333c15f161453b1a9865f2a
Joshua Yuan <yuanshisong211@126.com>
2018-01-22 17:26:49 +0800

    解决冲突

--- day10/07urllib.py
+++ day10/07urllib.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> 54da3e9ef366ae21d6cb7ee9c9e8a8a8e31f157a
 # 简单爬虫
 
 import urllib, gevent
@@ -9,7 +6,6 @@ from urllib import request
 
 
 def f(url):
-<<<<<<< HEAD
 	print('GET: %s' %url)
 	resp = request.urlopen(url)
 	data = resp.read()
@@ -22,16 +18,3 @@ gevent.joinall([
 	gevent.spawn(f, 'https://www.yahoo.com'),
 	gevent.spawn(f, 'https://www.github.com')
 ])
-=======
-    print('GET: %s' %url)
-    resp = request.urlopen(url)
-    data = resp.read()
-    print('%d bytes received from %s.' %(len(data), url))
-
-
-gevent.joinall([
-    gevent.spawn(f, 'https://www.python.org'),
-    gevent.spawn(f, 'https://www.yahoo.com'),
-    gevent.spawn(f, 'https://www.github.com')
-])
->>>>>>> 54da3e9ef366ae21d6cb7ee9c9e8a8a8e31f157a
